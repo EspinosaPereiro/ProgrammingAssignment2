5fadf81046c3e44d4aafefc6517c7fc8d4a17e9a
Juan Espinosa <juan.espinosa.pereiro@outlook.es>
2024-04-15 18:51:11 +0000

    Programming assignment week 2: inverse matrix cache

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Caching the inverse of a matrix
+#These two functions interact to provide the inverse of a given matrix. 
+#If the inverse has been calculated before, the second function can recover it from the cache.
 
-## Write a short comment describing this function
+#1. makeCacheMatrix: This function prints a list of four elements that subsequently set and get the
+#matrix and its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  setmatrix <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  getmatrix <- function() x
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() inv
+  list(setmatrix = setmatrix, getmatrix = getmatrix,
+       setinv = setinv,
+       getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+#2. cacheSolve: This function computes the inverse of the matrix returned by makeCacheMatrix above using the solve() function. 
+#When the inverse is not null (i.e., it was calculated before), the function recovers the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached inverse")
+    return(inv)
+  }
+  data <- x$getmatrix()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
+
+
+##Testing the two functions above
+#Example matrix
+n <- rnorm(16, 4, 1) #The solve() function will produce an error if the matrix determinant is 0.
+x <- matrix(n, 4, 4)
+
+#Applying the two functions subsequently, the cachesolve computes the inverse of the matrix
+x1 <- makeCacheMatrix(x)
+cacheSolve(x1)
+
+#After computing the matrix inverse, the second application of the function provides the inverse from cache
+cacheSolve(x1)
